0433c0eb7543d0821539f5b85365bcd64b3c554e
Tyler Hartland <tylerhartland7@gmail.com>
2025-10-23 22:30:23 -0500

    remove unused code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -44,34 +44,12 @@ public class Drive extends SubSystem {
         return driveRunner.done();
     }
 
-    public void drivePath(double[]... coordsList) {
-        TrajectoryActionBuilder builder = mecanumDrive.actionBuilder(getPose());
-        for (double[] coords : coordsList) {
-            builder = builder.splineToSplineHeading(new Pose2d(coords[0], coords[1], coords[2]), coords[2]);
-        }
-        Action action = builder.build();
-        driveRunner.drive(action);
-    }
-
     public void drivePath(Pose2d... poseList) {
         TrajectoryActionBuilder builder = mecanumDrive.actionBuilder(getPose());
         for (Pose2d pose : poseList) {
             builder = builder.splineToSplineHeading(pose, 0);
         }
         driveRunner.drive(builder.build());
-    }
-
-    public void splineUsingCoords() {
-        drivePath(
-                new double[]{24, 24, Math.PI / -2},
-                new double[]{0, 48, Math.PI * -1}
-        );
-    }
-
-    public void splineOneStep() {
-        drivePath(
-                new Pose2d(24, 24, Math.PI / 2)
-        );
     }
 
     public void splineSquiggleSquare() {
@@ -93,37 +71,6 @@ public class Drive extends SubSystem {
         drivePath(
                 new Pose2d(0, 0, 0)
         );
-    }
-
-    public void splineUsingPoses() {
-        drivePath(
-                new Pose2d(24, 24, Math.PI / 2),
-                new Pose2d(0, 48, Math.PI * 1)
-        );
-    }
-
-    public void blueFarScoreAThing() {
-        farScoreAThing(Alliance.blue);
-    }
-
-    public void redFarScoreAThing() {
-        farScoreAThing(Alliance.red);
-    }
-
-    public void farScoreAThing(Alliance alliance) {
-        drivePath(
-                alliance.pose(72, 0, Math.PI / 4),
-                alliance.pose(73, 1, Math.PI / 4)
-        );
-    }
-
-    public void splineBasic() {
-
-        Action action = mecanumDrive.actionBuilder(getPose())
-                .splineToSplineHeading(new Pose2d(24, 24, Math.PI / -2), Math.PI / -2)
-                .splineToSplineHeading(new Pose2d(0, 48, Math.PI * -1), Math.PI * -1)
-                .build();
-        driveRunner.drive(action);
     }
 
     private Pose2d getPose() {
