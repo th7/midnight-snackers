b0c301298a52a92d1b9fc0e519c7a85cf38c3e83
Tyler Hartland <tylerhartland7@gmail.com>
2026-01-02 22:31:52 -0600

    simplify stepdata callable handling; remove unneeded error declarations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/planrunner/Step.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/planrunner/Step.java
@@ -5,41 +5,28 @@ public class Step implements PlanPart {
     private long nanoStartedAt;
     private final VoidCallable start;
     private final Callable<Boolean> done;
-    private final StepDataCallable<Boolean> stepDataDone;
     private boolean started = false;
 
     public Step(String name, VoidCallable start, Callable<Boolean> done) {
         this.name = name;
         this.start = start;
         this.done = done;
-        this.stepDataDone = null;
     }
 
     public Step(String name, VoidCallable start, StepDataCallable<Boolean> stepDataDone) {
         this.name = name;
         this.start = start;
-        this.done = null;
-        this.stepDataDone = stepDataDone;
+        this.done = () -> stepDataDone.call(new StepData(nanoStartedAt));
     }
 
     public boolean done() {
         if (!started) {
-            try {
-                start.call();
-            } catch (IllegalAccessException | InstantiationException e) {
-                throw new RuntimeException(e);
-            }
+            start.call();
             started = true;
             nanoStartedAt = System.nanoTime();
         }
 
-        if (this.stepDataDone != null) {
-            return this.stepDataDone.call(new StepData(System.nanoTime()));
-        }
-        if (this.done != null) {
-            return this.done.call();
-        }
-
+        return this.done.call();
     }
 
     public String currentStep() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/planrunner/VoidCallable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/planrunner/VoidCallable.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode.planrunner;
 
 public interface VoidCallable {
-    void call() throws IllegalAccessException, InstantiationException;
+    void call();
 }
