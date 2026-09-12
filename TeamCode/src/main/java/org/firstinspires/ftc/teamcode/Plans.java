60faadecde712bbf5ca6a39d800430b01da8764f
ty <ty@coding-server.invalid>
2026-09-12 18:17:06 -0500

    test commit 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Plans.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Plans.java
@@ -9,7 +9,6 @@ import org.firstinspires.ftc.teamcode.planrunner.Plan;
 import org.firstinspires.ftc.teamcode.planrunner.PlanPart;
 import org.firstinspires.ftc.teamcode.planrunner.Step;
 
-
 public class Plans extends SuperSystem {
     private final Brain brain;
     private double startedWaitAt;
